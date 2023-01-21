f32738110d0dcc172aa4218a37bd5d28cc678d97
Luigi Vismara <luigivis98@gmail.com>
2023-01-21 13:05:11 -0500

    fix: se ajusta .sh

--- execute.sh
+++ execute.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
-echo "AutoSys"
+echo "***************************************"
+echo "*++++++AutoSys Generation Date*********"
+echo "***************************************"
 echo $(date)
 
 echo
+echo "***************************************"
+echo "*++++++Executing BUILD and run ********"
+echo "***************************************"
+cd autosys/
+npm install
 
-echo "Generating .app.config.json"
-cat <<EOF >autosys/.app.config.json
-{
-  apps : [
-    {
-      name      : "autosys-frontend",
-      script    : "npm",
-      interpreter: "none",
-      args: "serve -s build -p 7000"
-    }
-  ]
-}
+echo
+echo "***************************************"
+echo "******Generating environment file******"
+echo "***************************************"
+cat <<EOF >.env
+PORT=7000
 EOF
 
+echo
+echo "***************************************"
+echo "****** Running AutoSys PM2 ************"
+echo "***************************************"
+
+echo
+
+echo "Stop Module"
+pm2 stop AutoSys-Frontend:7000
+
+echo "Stop Module"
+pm2 delete AutoSys-Frontend:7000
+
+echo "Starting"
+pm2 start --name=AutoSys-Frontend:7000 npm -- start
 pm2 start autosys/.app.config.json
+
+cd ../
