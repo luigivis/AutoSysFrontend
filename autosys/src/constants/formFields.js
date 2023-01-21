82d22e465f2077b2732c120682ab78723c0ff169
Bymatt10 <matthewreyesvanegasma@hotmail.com>
2023-01-20 22:30:02 -0600

    Feat: Login semifunctional

---
+++ autosys/src/constants/formFields.js
@@ -0,0 +1,23 @@
+const loginFields=[
+    {
+        labelText:"User",
+        labelFor:"user",
+        id:"username",
+        name:"username",
+        type:"user",
+        autoComplete:"user",
+        isRequired:true,
+        placeholder:"User"
+    },
+    {
+        labelText:"Password",
+        labelFor:"password",
+        id:"password",
+        name:"password",
+        type:"password",
+        autoComplete:"current-password",
+        isRequired:true,
+        placeholder:"Password"
+    }
+]
+export {loginFields}
